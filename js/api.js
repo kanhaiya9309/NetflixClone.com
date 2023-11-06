a76d6c193dbffcc87a3443022eaef3f075bd11f9
Kanhaiya Gawali <124244655+kanhaiya9309@users.noreply.github.com>
2023-11-06 20:16:32 +0530

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,7 @@
+let api_key = "79e114cce9060f23c98a1693dbd765f5";
+
+let img_Url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+
